2a105b64c3c8078c93e1db87866df5f68e6a86e6
Diego Hernandez <ishtaracademy@outlook.com>
2020-08-29 01:45:20 -0600

    All dom elements are updating based on AJAX requests

--- Assets/script.js
+++ Assets/script.js
@@ -3,18 +3,23 @@
     let activeCity;
     // Blank object to contain local storage items
     let recCities = [];
+    // Store current time
+    let date = moment();
+    // Contain data from current weather API
     let weathData = {
         temp: "",
         hum: "",
         wind: "",
         lat: "",
         lon: "",
+        uv: "",
+        icon: "",
     }
 
 
 // * Functions
 
-    // Checks Local Storage for Data, populates recCities, and calls renderCities
+    // Checks Local Storage for Data, populates recCities array, and calls renderCities()
     function init() {
         // ** Parse Data In Local Storage
         let storedCities = JSON.parse(localStorage.getItem("cities"))
@@ -23,7 +28,8 @@
         if (storedCities !== null) {
             recCities = storedCities; 
         }
-        renderCities()
+        $('#current-city').text(date.format('MMMM Do'));
+        renderCities();
     }
 
     // Render Cities from recCities object
@@ -32,71 +38,127 @@
         $('#rec-search').html("")
         // ** Create HTML elements and send to DOM
         for (let i = 0; i < recCities.length; i++) {
-            var newCity = $('<li>');
+            var newCity = $('<li>')
             newCity.prepend($('<button>').addClass('btn btn-secondary').text(recCities[i]));
-            $('#rec-search').prepend(newCity)
-            // console.dir($('#rec-search').children());   
+            $('#rec-search').prepend(newCity);
+            // console.dir($('#rec-search').children()); 
+            checkList();
         }       
     }
 
     // Make AJAX Requests
     function weatherRequest(city) {
         // *** Create URLs and store in variable
-        let curWea = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d203ab0e9d06e58c82c3b764c42b7aa7"
-        // let fiveWea = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=d203ab0e9d06e58c82c3b764c42b7aa7"
-        // let uvWea = "https://api.openweathermap.org/data/2.5/uvi?appid=d203ab0e9d06e58c82c3b764c42b7aa7&lat=" + lat + "&lon=" + lon;
-
+        let curWea = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d203ab0e9d06e58c82c3b764c42b7aa7";
+        
         // *** Make AJAX Request for current weather
         $.ajax({
             url: curWea,
             method: "GET"
-        }) .then (function current(curResponce) {
-            console.log(curResponce);
-            weathData.temp = curResponce.main.temp
-            weathData.hum = curResponce.main.humidity
-            weathData.wind = curResponce.wind.speed
-            weathData.lat = curResponce.coord.lat
-            weathData.lon = curResponce.coord.lon
-            console.log(weathData);        
-        })
+        }) .then (function current(curRespon) {
+            console.log(curRespon);
+            weathData.temp = Math.floor((curRespon.main.temp - 273.15) * 1.80 + 32);
+            weathData.hum = curRespon.main.humidity;
+            weathData.wind = curRespon.wind.speed;
+            weathData.lat = curRespon.coord.lat;
+            weathData.lon = curRespon.coord.lon;
+            weathData.icon = "http://openweathermap.org/img/wn/" + curRespon.weather[0].icon + "@2x.png"
+            console.log(weathData);
+            $.ajax({
+                url: "https://api.openweathermap.org/data/2.5/uvi?appid=d203ab0e9d06e58c82c3b764c42b7aa7&lat=" + weathData.lat + "&lon=" + weathData.lon,
+                method: "GET"
+            }) .then (function (uvRespon){
+                console.log(uvRespon);
+                weathData.uv = uvRespon.value;
+                updateCurrent();
+
+                $.ajax({
+                    url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + weathData.lat + "&lon=" + weathData.lon + "&appid=d203ab0e9d06e58c82c3b764c42b7aa7",
+                    method: "GET"
+                }) .then (function(fiResponce) {
+                    console.log(fiResponce);
+                    updateFive(fiResponce);
+                    renderCities();
+                });
+            })
+        });       
+    }
+
+
+    // updateCurrent()
+        // Update <h2> Content
+    function updateCurrent() {
+        console.dir(document.querySelector('#temp'));
+        $('#current-city').html("" + activeCity + ": " + "<span>" + date.format('MMM. Do') + "<span><img src='"+ weathData.icon + "'>" );
+        console.log(weathData.uv);
+        $('#temp').text(weathData.temp + " °F");
+        $('#hum').text(weathData.hum + "%");
+        $('#wind').text(weathData.wind + " MPH");
+        $('#uv').text(weathData.uv);
+    }
+
+    function updateFive(data) {
+        console.log(data);
+        console.dir($('.card-body'));
+        for (let i = 1; i < 6; i++) {
+            let currCard = $('.card-body')[i].children;
+                // console.log(currCard[0].textContent);
+                console.log($('.card-body')[i]);
+                let currDay = data.daily[i];
+                console.log(currDay);
+                date = moment();
+                currCard[0].textContent = date.add(''+i+'', 'day').format('dddd');
+                // console.log(currDay.temp.day);
+                currCard[1].setAttribute('src', "http://openweathermap.org/img/wn/" + currDay.weather[0].icon + "@2x.png");
+                currCard[2].textContent = "Temperature: " + Math.floor((currDay.temp.day - 273.15) * 1.80 + 32);
+                currCard[3].textContent = "Humidity: " + currDay.humidity; 
+        } 
     }
 
 
 // * Run When Page Loads To Update recCities with content in local storage, then call renderCities
 init();
 
-// * Grab the value from user input; run function to append to ul; run function that gets AJAX request
+// * Grab the value from user input; run renderCities; run weatherRequest()
+    // weatherReqest() runs updateCurrent() and updateFive()
 
 $('#search-btn').click(function(event){
-    event.preventDefault()
-    activeCity = $('#user-search').val().trim()   
+    event.preventDefault();
+    activeCity = $('#user-search').val().trim();
             // ^ Test paths and variable         
             console.log($('#user-search').val());
             console.log(activeCity);
     recCities.push(activeCity);
             console.log(recCities);
-    localStorage.setItem('cities', JSON.stringify(recCities))
-    renderCities()
+    localStorage.setItem('cities', JSON.stringify(recCities));
+    
     
     weatherRequest(activeCity);   
 });
 
+// * Grab Inner Text from Recent Search Buttons and make ajax request
 
-// * Function that appends <li> to Recent Searches and store in an array??
-        // Keep that array in local storage, so that another funtion can load them in when the page runs? 
-// function renderCities(city) {
-//             console.log(city);
-//     let newCity = $('<li>')
-//     newCity.prepend($('<button>').addClass('btn btn-secondary').text(city));
-//     $('#rec-search').prepend(newCity)
-//     storedCities.push(city)
-//             console.log(storedCities);
-// }
+$('#rec-search').click(function(event){
+        console.log($(event.target).text());
+    activeCity = $(event.target).text();
+    weatherRequest(activeCity);
+})
+
+// Check the lenght of <ul> 
 
 
+function checkList () {
+    let checker = $('#rec-search')[0].children.length;
 
-// * Store cities in local storage
-// function storeCity(city)
+    if (checker >= 11) {
+        console.log("Balls");
+        recCities.splice(0,1);
+    }
 
-// * Function That Runs AJAX
+    }
+
+
+
+
+
 
